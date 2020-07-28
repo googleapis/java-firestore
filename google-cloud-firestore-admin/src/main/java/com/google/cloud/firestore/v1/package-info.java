03ef755dd164e6f1ec749f3f985b913b5ae23d14
BenWhitehead <BenWhitehead@users.noreply.github.com>
2020-07-28 13:26:33 -0400

    chore!: move FirestoreAdminClient and associated classes to new artifact google-cloud-firestore-admin (#311)
    * All classes are retained in the new artifact
    * New version defined in versions.txt for new artifact
      * For now, version number is set to be the same as google-cloud-firestore

---
+++ google-cloud-firestore-admin/src/main/java/com/google/cloud/firestore/v1/package-info.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A client to Google Cloud Firestore Admin API.
+ *
+ * <p>The interfaces provided are listed below, along with usage samples.
+ *
+ * <p>==================== FirestoreAdminClient ====================
+ *
+ * <p>Service Description: Operations are created by service `FirestoreAdmin`, but are accessed via
+ * service `google.longrunning.Operations`.
+ *
+ * <p>Sample for FirestoreAdminClient:
+ *
+ * <pre>
+ * <code>
+ * try (FirestoreAdminClient firestoreAdminClient = FirestoreAdminClient.create()) {
+ *   IndexName name = IndexName.of("[PROJECT]", "[DATABASE]", "[COLLECTION]", "[INDEX]");
+ *   firestoreAdminClient.deleteIndex(name);
+ * }
+ * </code>
+ * </pre>
+ */
+@Generated("by gapic-generator")
+package com.google.cloud.firestore.v1;
+
+import javax.annotation.Generated;
