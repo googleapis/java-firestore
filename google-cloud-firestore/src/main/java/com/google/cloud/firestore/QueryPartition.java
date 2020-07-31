0f1ef4711d785790639250c7d846af4a2d524d31
Sebastian Schmidt <mrschmidt@google.com>
2020-07-31 09:59:52 -0700

    Review

--- google-cloud-firestore/src/main/java/com/google/cloud/firestore/QueryPartition.java
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/QueryPartition.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.firestore;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * A split point that can be used by in a query as a starting or end point for the query results.
@@ -57,5 +75,23 @@ public class QueryPartition {
       baseQuery = baseQuery.endBefore(endBefore);
     }
     return baseQuery;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof QueryPartition)) return false;
+    QueryPartition partition = (QueryPartition) o;
+    return query.equals(partition.query) &&
+            Arrays.equals(startAt, partition.startAt) &&
+            Arrays.equals(endBefore, partition.endBefore);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Objects.hash(query);
+    result = 31 * result + Arrays.hashCode(startAt);
+    result = 31 * result + Arrays.hashCode(endBefore);
+    return result;
   }
 }
