bb65887037e40779b4e7f7150e37c903ad502bba
Wu-Hui <wu.hui.github@gmail.com>
2020-09-15 09:44:22 -0400

    Use SDK version management and other changes.

--- update.sh
+++ update.sh
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+# It is an error if a variable is not set.
+set -u
 IFS=$'\n\t'
 
 # Variables
