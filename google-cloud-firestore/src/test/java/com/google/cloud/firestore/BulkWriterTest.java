1f7b0fce059bbd604bc34d66c5fc4268f0981131
Brian Chen <chenbrian@google.com>
2020-11-12 19:10:36 -0600

    add back timeout rule to test

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/BulkWriterTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/BulkWriterTest.java
@@ -52,7 +52,9 @@ import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -66,7 +68,7 @@ import org.mockito.stubbing.Answer;
 @RunWith(MockitoJUnitRunner.class)
 public class BulkWriterTest {
 
-  //  @Rule public Timeout timeout = new Timeout(500, TimeUnit.MILLISECONDS);
+  @Rule public Timeout timeout = new Timeout(500, TimeUnit.MILLISECONDS);
 
   @Spy private final FirestoreRpc firestoreRpc = Mockito.mock(FirestoreRpc.class);
 
