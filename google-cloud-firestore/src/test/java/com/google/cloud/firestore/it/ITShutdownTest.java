734bd7e4e450c10b7e689e11a7a30871680b28c0
Sebastian Schmidt <mrschmidt@google.com>
2021-06-23 21:07:13 -0600

    Add test helper

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITShutdownTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITShutdownTest.java
@@ -21,105 +21,64 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreException;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.ListenerRegistration;
+import com.google.cloud.firestore.LocalFirestoreHelper;
 import com.google.cloud.firestore.QuerySnapshot;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.junit.rules.Timeout;
 
 public class ITShutdownTest {
   @Rule public final Timeout timeout = new Timeout(5, TimeUnit.SECONDS);
+  @Rule public TestName testName = new TestName();
 
   @Test
   public void closeSuccess_withListenerRemove() throws Exception {
-    final CountDownLatch cdl = new CountDownLatch(1);
-
     Firestore fs = FirestoreOptions.getDefaultInstance().getService();
-    ListenerRegistration listener =
-        fs.collection("abcd")
-            .addSnapshotListener(
-                new EventListener<QuerySnapshot>() {
-                  @Override
-                  public void onEvent(
-                      @Nullable QuerySnapshot value, @Nullable FirestoreException error) {
-                    cdl.countDown();
-                  }
-                });
-
-    cdl.await();
+    ListenerRegistration listener = attachListener(fs);
     listener.remove();
     fs.close();
   }
 
   @Test
   public void closeSuccess_withoutListenerRemove() throws Exception {
-    final CountDownLatch cdl = new CountDownLatch(1);
-
     Firestore fs = FirestoreOptions.getDefaultInstance().getService();
-    ListenerRegistration listener =
-        fs.collection("abcd")
-            .addSnapshotListener(
-                new EventListener<QuerySnapshot>() {
-                  @Override
-                  public void onEvent(
-                      @Nullable QuerySnapshot value, @Nullable FirestoreException error) {
-                    cdl.countDown();
-                  }
-                });
-
-    cdl.await();
+    attachListener(fs);
     fs.close();
   }
 
   @Test
   public void shutdownNowSuccess_withoutListenerRemove() throws Exception {
-    final CountDownLatch cdl = new CountDownLatch(1);
-
     Firestore fs = FirestoreOptions.getDefaultInstance().getService();
-    ListenerRegistration listener =
-        fs.collection("abcd")
-            .addSnapshotListener(
-                new EventListener<QuerySnapshot>() {
-                  @Override
-                  public void onEvent(
-                      @Nullable QuerySnapshot value, @Nullable FirestoreException error) {
-                    cdl.countDown();
-                  }
-                });
-
-    cdl.await();
+    attachListener(fs);
     fs.shutdownNow();
   }
 
   @Test
   public void shutdownSuccess_withoutListenerRemove() throws Exception {
-    final CountDownLatch cdl = new CountDownLatch(1);
-
     Firestore fs = FirestoreOptions.getDefaultInstance().getService();
-    ListenerRegistration listener =
-        fs.collection("abcd")
-            .addSnapshotListener(
-                new EventListener<QuerySnapshot>() {
-                  @Override
-                  public void onEvent(
-                      @Nullable QuerySnapshot value, @Nullable FirestoreException error) {
-                    cdl.countDown();
-                  }
-                });
-
-    cdl.await();
+    attachListener(fs);
     fs.shutdown();
   }
 
   @Test
   public void closeAndShutdown() throws Exception {
-    final CountDownLatch cdl = new CountDownLatch(1);
-
     Firestore fs = FirestoreOptions.getDefaultInstance().getService();
-    ListenerRegistration listener =
-        fs.collection("abcd")
+    attachListener(fs);
+    fs.close();
+    fs.shutdown();
+    fs.shutdownNow();
+  }
+
+  private ListenerRegistration attachListener(Firestore fs) throws InterruptedException {
+    final CountDownLatch cdl = new CountDownLatch(1);
+    ListenerRegistration listenerRegistration =
+        fs.collection(
+                String.format(
+                    "java-%s-%s", testName.getMethodName(), LocalFirestoreHelper.autoId()))
             .addSnapshotListener(
                 new EventListener<QuerySnapshot>() {
                   @Override
@@ -128,10 +87,7 @@ public class ITShutdownTest {
                     cdl.countDown();
                   }
                 });
-
     cdl.await();
-    fs.close();
-    fs.shutdown();
-    fs.shutdownNow();
+    return listenerRegistration;
   }
 }
