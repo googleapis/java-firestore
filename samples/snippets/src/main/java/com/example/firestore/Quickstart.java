b9360214fa13956bd2e1812250bd581073ec758b
Christopher Wilcox <crwilcox@google.com>
2021-07-01 16:28:48 -0700

    chore: modify sample region tag to more closely match others (#687)
    cl 382617864

--- samples/snippets/src/main/java/com/example/firestore/Quickstart.java
+++ samples/snippets/src/main/java/com/example/firestore/Quickstart.java
@@ -21,8 +21,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.DocumentReference;
 // [START fs_include_dependencies]
 // [START firestore_setup_dependencies]
+// [START firestore_deps]
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
+// [END firestore_deps]
 // [END firestore_setup_dependencies]
 // [END fs_include_dependencies]
 import com.google.cloud.firestore.QueryDocumentSnapshot;
