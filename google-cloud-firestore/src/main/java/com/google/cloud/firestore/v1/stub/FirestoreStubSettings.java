68b15944e6668ff53bedcd0e7f8558c319a6ad68
yoshi-automation <yoshi-automation@google.com>
2021-07-29 14:59:18 -0700

    chore: release gapic-generator-java v1.0.12
    Committer: @miraleung
    PiperOrigin-RevId: 378718217
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jun 10 13:08:49 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 08c4eeb531c01ad031134dca94b18e3f7dd35902
    Source-Link: https://github.com/googleapis/googleapis/commit/08c4eeb531c01ad031134dca94b18e3f7dd35902

--- google-cloud-firestore/src/main/java/com/google/cloud/firestore/v1/stub/FirestoreStubSettings.java
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/v1/stub/FirestoreStubSettings.java
@@ -414,6 +414,11 @@ public class FirestoreStubSettings extends StubSettings<FirestoreStubSettings> {
     return "firestore.googleapis.com:443";
   }
 
+  /** Returns the default mTLS service endpoint. */
+  public static String getDefaultMtlsEndpoint() {
+    return "firestore.mtls.googleapis.com:443";
+  }
+
   /** Returns the default service scopes. */
   public static List<String> getDefaultServiceScopes() {
     return DEFAULT_SERVICE_SCOPES;
@@ -704,6 +709,8 @@ public class FirestoreStubSettings extends StubSettings<FirestoreStubSettings> {
       builder.setCredentialsProvider(defaultCredentialsProviderBuilder().build());
       builder.setInternalHeaderProvider(defaultApiClientHeaderProviderBuilder().build());
       builder.setEndpoint(getDefaultEndpoint());
+      builder.setMtlsEndpoint(getDefaultMtlsEndpoint());
+      builder.setSwitchToMtlsEndpointAllowed(true);
 
       return initDefaults(builder);
     }
