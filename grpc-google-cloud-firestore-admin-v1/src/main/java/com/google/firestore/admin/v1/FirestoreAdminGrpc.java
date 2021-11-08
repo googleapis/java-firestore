418f5ff9c968da8b52bc80f57144d28024a493b9
yoshi-automation <yoshi-automation@google.com>
2021-11-08 14:52:47 -0800

    chore: update Java and Python dependencies
    PiperOrigin-RevId: 408420890
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Nov 8 13:03:45 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 2921f9fb3bfbd16f6b2da0104373e2b47a80a65e
    Source-Link: https://github.com/googleapis/googleapis/commit/2921f9fb3bfbd16f6b2da0104373e2b47a80a65e

--- grpc-google-cloud-firestore-admin-v1/src/main/java/com/google/firestore/admin/v1/FirestoreAdminGrpc.java
+++ grpc-google-cloud-firestore-admin-v1/src/main/java/com/google/firestore/admin/v1/FirestoreAdminGrpc.java
@@ -28,6 +28,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 @javax.annotation.Generated(
     value = "by gRPC proto compiler",
     comments = "Source: google/firestore/admin/v1/firestore_admin.proto")
+@io.grpc.stub.annotations.GrpcGenerated
 public final class FirestoreAdminGrpc {
 
   private FirestoreAdminGrpc() {}
