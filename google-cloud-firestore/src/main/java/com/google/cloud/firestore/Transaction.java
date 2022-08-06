8c4086672ab5e95ccf189ada060394ce0aca2751
Denver Coneybeare <dconeybe@google.com>
2022-08-05 22:55:32 -0400

    Transaction.java: get(AggregateQuery) added

--- google-cloud-firestore/src/main/java/com/google/cloud/firestore/Transaction.java
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/Transaction.java
@@ -190,4 +190,10 @@ public final class Transaction extends UpdateBuilder<Transaction> {
 
     return query.get(transactionId);
   }
+
+  @Nonnull
+  public ApiFuture<AggregateQuerySnapshot> get(@Nonnull AggregateQuery query) {
+    throw new RuntimeException("not implemented");
+  }
+
 }
