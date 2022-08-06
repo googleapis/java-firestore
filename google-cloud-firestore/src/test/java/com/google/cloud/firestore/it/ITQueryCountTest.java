d999721f6131e4b11893131c2f2a7c62bc4d28a8
Denver Coneybeare <dconeybe@google.com>
2022-08-05 23:41:11 -0400

    ITQueryCountTest.java: add tests for snapshot query and read time, as well as some minor refactoring

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITQueryCountTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITQueryCountTest.java
@@ -16,29 +16,9 @@
 
 package com.google.cloud.firestore.it;
 
-import com.google.api.core.ApiFuture;
-import com.google.api.core.ApiFutures;
-import com.google.api.core.SettableApiFuture;
-import com.google.api.gax.retrying.RetrySettings;
-import com.google.api.gax.rpc.ApiStreamObserver;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.*;
-import com.google.cloud.firestore.LocalFirestoreHelper.*;
-import com.google.cloud.firestore.Query.Direction;
-import com.google.cloud.firestore.Transaction.Function;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.google.firestore.bundle.BundleElement;
-import com.google.firestore.bundle.BundledDocumentMetadata;
-import com.google.firestore.bundle.BundledQuery.LimitType;
-import com.google.firestore.bundle.NamedQuery;
-import com.google.firestore.v1.RunQueryRequest;
-import io.grpc.Status;
-import io.grpc.Status.Code;
-import io.grpc.StatusRuntimeException;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -46,18 +26,12 @@ import org.junit.Test;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.threeten.bp.Duration;
 
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
-import static com.google.cloud.firestore.FirestoreBundleTest.*;
-import static com.google.cloud.firestore.LocalFirestoreHelper.*;
 import static com.google.common.truth.Truth.assertThat;
-import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class ITQueryCountTest {
@@ -84,14 +58,14 @@ public class ITQueryCountTest {
   public void countShouldReturnZeroForEmptyCollection() throws Exception {
     CollectionReference collection = createCollectionWithDocuments(0);
     AggregateQuerySnapshot snapshot = collection.count().get().get();
-    assertEquals(Long.valueOf(0), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(0);
   }
 
   @Test
   public void countShouldReturnNumDocumentsInNonEmptyCollection() throws Exception {
     CollectionReference collection = createCollectionWithDocuments(5);
     AggregateQuerySnapshot snapshot = collection.count().get().get();
-    assertEquals(Long.valueOf(5), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(5);
   }
 
   @Test
@@ -100,21 +74,21 @@ public class ITQueryCountTest {
     createDocumentsWithKeyValuePair(collection, 3, "key", 42);
     createDocumentsWithKeyValuePair(collection, 5, "key", 24);
     AggregateQuerySnapshot snapshot = collection.whereEqualTo("key", 42).count().get().get();
-    assertEquals(Long.valueOf(3), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(3);
   }
 
   @Test
   public void countShouldRespectLimit() throws Exception {
     CollectionReference collection = createCollectionWithDocuments(5);
     AggregateQuerySnapshot snapshot = collection.limit(2).count().get().get();
-    assertEquals(Long.valueOf(2), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(2);
   }
 
   @Test
   public void countShouldRespectOffset() throws Exception {
     CollectionReference collection = createCollectionWithDocuments(5);
     AggregateQuerySnapshot snapshot = collection.offset(2).count().get().get();
-    assertEquals(Long.valueOf(3), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(3);
   }
 
   @Test
@@ -125,7 +99,7 @@ public class ITQueryCountTest {
     createDocumentsWithKeyValuePair(collection, 4, "key1", 99);
     AggregateQuerySnapshot snapshot = collection.orderBy("key1").count().get().get();
     // Note: A subtle side effect of order-by is that it filters out documents that do not have the order-by field.
-    assertEquals(Long.valueOf(7), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(7);
   }
 
   @Test
@@ -133,7 +107,28 @@ public class ITQueryCountTest {
     CollectionReference collection = createCollectionWithDocuments(10);
     List<QueryDocumentSnapshot> documentSnapshots = collection.get().get().getDocuments();
     AggregateQuerySnapshot snapshot = collection.startAt(documentSnapshots.get(2)).endAt(documentSnapshots.get(7)).count().get().get();
-    assertEquals(Long.valueOf(6), snapshot.getCount());
+    assertThat(snapshot.getCount()).isEqualTo(6);
+  }
+
+  @Test
+  public void aggregateSnapshotShouldHaveReasonableReadTime() throws Exception {
+    CollectionReference collection = createCollectionWithDocuments(5);
+    AggregateQuerySnapshot snapshot1 = collection.count().get().get();
+    AggregateQuerySnapshot snapshot2 = collection.count().get().get();
+    long readTimeMs1 = msFromTimestamp(snapshot1.getReadTime());
+    long readTimeMs2 = msFromTimestamp(snapshot2.getReadTime());
+    assertThat(readTimeMs1).isLessThan(readTimeMs2);
+    assertThat(readTimeMs2 - readTimeMs1).isLessThan(500);
+  }
+
+  @Test
+  public void aggregateSnapshotShouldHaveCorrectQuery() throws Exception {
+    CollectionReference collection = createCollectionWithDocuments(5);
+    AggregateQuery aggregateQuery = collection.count();
+    AggregateQuerySnapshot snapshot1 = aggregateQuery.get().get();
+    AggregateQuerySnapshot snapshot2 = aggregateQuery.get().get();
+    assertThat(snapshot1.getQuery()).isSameInstanceAs(aggregateQuery);
+    assertThat(snapshot2.getQuery()).isSameInstanceAs(aggregateQuery);
   }
 
   private CollectionReference createEmptyCollection() {
@@ -147,14 +142,17 @@ public class ITQueryCountTest {
     return collection;
   }
 
-  private CollectionReference createDocumentsWithKeyValuePair(CollectionReference collection, int numDocumentsToCreate, String key, int value) throws ExecutionException, InterruptedException {
+  private void createDocumentsWithKeyValuePair(CollectionReference collection, int numDocumentsToCreate, String key, int value) throws ExecutionException, InterruptedException {
     for (int i=0; i<numDocumentsToCreate; i++) {
       DocumentReference doc = collection.document();
       HashMap<String, Object> data = new HashMap<>();
       data.put(key, value);
       doc.set(data).get();
     }
-    return collection;
+  }
+
+  private static long msFromTimestamp(Timestamp timestamp) {
+    return (timestamp.getSeconds() * 1_000) + (timestamp.getNanos() / 1_000_000);
   }
 
 }
