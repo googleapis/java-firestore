a51d5cabb2db6f58b07cb9df53860a8988394eec
kolea2 <45548808+kolea2@users.noreply.github.com>
2022-08-25 13:20:13 -0400

    samples: fix beam samples to run from command line (#1017)

--- samples/snippets/src/main/java/com/example/firestore/beam/ExampleFirestoreBeamRead.java
+++ samples/snippets/src/main/java/com/example/firestore/beam/ExampleFirestoreBeamRead.java
@@ -23,7 +23,6 @@ import com.google.firestore.v1.RunQueryResponse;
 import com.google.firestore.v1.StructuredQuery;
 import com.google.firestore.v1.Value;
 import java.util.Objects;
-import java.util.UUID;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.firestore.FirestoreIO;
@@ -39,7 +38,7 @@ import org.apache.beam.sdk.values.PCollection;
 public class ExampleFirestoreBeamRead {
 
   public static void main(String[] args) {
-    runRead(args, "cities-collection-" + UUID.randomUUID().toString().substring(0, 10));
+    runRead(args, "cities-beam-sample");
   }
 
   public static void runRead(String[] args, String collectionId) {
