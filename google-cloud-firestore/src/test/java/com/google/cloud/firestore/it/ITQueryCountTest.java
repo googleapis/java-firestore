77e1b26fb3ebe3fe04531f0f1fe8777a6e847878
Denver Coneybeare <dconeybe@google.com>
2022-10-22 01:22:53 -0400

    test: Verify count query error message when missing index

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITQueryCountTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITQueryCountTest.java
@@ -20,6 +20,7 @@ import static com.google.cloud.firestore.LocalFirestoreHelper.autoId;
 import static com.google.common.truth.Truth.assertThat;
 import static java.util.Collections.singletonMap;
 import static org.junit.Assert.assertThrows;
+import static org.junit.Assume.assumeFalse;
 import static org.junit.Assume.assumeTrue;
 
 import com.google.api.core.ApiFuture;
@@ -309,6 +310,25 @@ public class ITQueryCountTest {
 
     Long transactionCount = transactionFuture.get();
     assertThat(transactionCount).isEqualTo(5);
+  }
+
+  @Test
+  public void aggregateQueryShouldFailWithGoodMessageIfMissingIndex() {
+    assumeFalse(
+        "Skip this test when running against the Firestore emulator because the Firestore emulator "
+            + "does not use indexes and never fails with a 'missing index' error",
+        isRunningAgainstFirestoreEmulator());
+
+    CollectionReference collection = createEmptyCollection();
+    Query compositeIndexQuery = collection.whereEqualTo("field1", 42).whereLessThan("field2", 99);
+    AggregateQuery compositeIndexCountQuery = compositeIndexQuery.count();
+    ApiFuture<AggregateQuerySnapshot> future = compositeIndexCountQuery.get();
+
+    ExecutionException executionException = assertThrows(ExecutionException.class, future::get);
+
+    Throwable throwable = executionException.getCause();
+    assertThat(throwable).hasMessageThat().ignoringCase().contains("index");
+    assertThat(throwable).hasMessageThat().contains("https://console.firebase.google.com");
   }
 
   private CollectionReference createEmptyCollection() {
