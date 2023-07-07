1b2d3e1e8c62a1e116a16b5b08dc2fff770fd7a3
milaGGL <107142260+milaGGL@users.noreply.github.com>
2023-07-07 18:11:56 -0400

    add more comments

--- google-cloud-firestore/src/main/java/com/google/cloud/firestore/collection/ImmutableSortedMap.java
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/collection/ImmutableSortedMap.java
@@ -27,40 +27,141 @@ import java.util.Map;
 @InternalApi
 public abstract class ImmutableSortedMap<K, V> implements Iterable<Map.Entry<K, V>> {
 
+  /**
+   * Checks if the map contains the specified key.
+   *
+   * @param key the key to check
+   * @return {@code true} if the map contains the key, {@code false} otherwise
+   */
   public abstract boolean containsKey(K key);
 
+  /**
+   * Retrieves the value associated with the specified key.
+   *
+   * @param key the key to retrieve the value for
+   * @return the value associated with the key, or {@code null} if the key is not present in the map
+   */
   public abstract V get(K key);
 
+  /**
+   * Removes the specified key and its associated value from the map.
+   *
+   * @param key the key to remove
+   * @return a new {@code ImmutableSortedMap} instance with the specified key and value removed
+   */
   public abstract ImmutableSortedMap<K, V> remove(K key);
 
+  /**
+   * Inserts a new key-value pair into the map.
+   *
+   * @param key the key to insert
+   * @param value the value associated with the key
+   * @return a new {@code ImmutableSortedMap} instance with the specified key and value inserted
+   */
   public abstract ImmutableSortedMap<K, V> insert(K key, V value);
 
+  /**
+   * Retrieves the minimum key in the map.
+   *
+   * @return the minimum key in the map
+   */
   public abstract K getMinKey();
 
+  /**
+   * Retrieves the maximum key in the map.
+   *
+   * @return the maximum key in the map
+   */
   public abstract K getMaxKey();
 
+  /**
+   * Returns the size of the map.
+   *
+   * @return the number of key-value pairs in the map
+   */
   public abstract int size();
 
+  /**
+   * Checks if the map is empty.
+   *
+   * @return {@code true} if the map is empty, {@code false} otherwise
+   */
   public abstract boolean isEmpty();
 
+  /**
+   * Performs an in-order traversal of the map and applies the specified visitor to each node.
+   *
+   * @param visitor the visitor to apply to each node in the map
+   */
   public abstract void inOrderTraversal(LLRBNode.NodeVisitor<K, V> visitor);
 
+  /**
+   * Returns an iterator over the entries in the map.
+   *
+   * @return an iterator over the entries in the map
+   */
   public abstract Iterator<Map.Entry<K, V>> iterator();
 
+  /**
+   * Returns an iterator over the entries in the map starting from the specified key.
+   *
+   * @param key the key to start the iterator from
+   * @return an iterator over the entries in the map starting from the specified key
+   */
   public abstract Iterator<Map.Entry<K, V>> iteratorFrom(K key);
 
+  /**
+   * Returns a reverse iterator over the entries in the map starting from the specified key.
+   *
+   * @param key the key to start the reverse iterator from
+   * @return a reverse iterator over the entries in the map starting from the specified key
+   */
   public abstract Iterator<Map.Entry<K, V>> reverseIteratorFrom(K key);
 
+  /**
+   * Returns a reverse iterator over the entries in the map.
+   *
+   * @return a reverse iterator over the entries in the map
+   */
   public abstract Iterator<Map.Entry<K, V>> reverseIterator();
 
+  /**
+   * Retrieves the predecessor key of the specified key.
+   *
+   * @param key the key to find the predecessor for
+   * @return the predecessor key of the specified key, or {@code null} if there is no predecessor
+   */
   public abstract K getPredecessorKey(K key);
 
+  /**
+   * Retrieves the successor key of the specified key.
+   *
+   * @param key the key to find the successor for
+   * @return the successor key of the specified key, or {@code null} if there is no successor
+   */
   public abstract K getSuccessorKey(K key);
 
+  /**
+   * Returns the index of the specified key in the map.
+   *
+   * @param key the key to find the index for
+   * @return the index of the specified key, or {@code -1} if the key is not present in the map
+   */
   public abstract int indexOf(K key);
 
+  /**
+   * Returns the comparator used to sort the keys in the map.
+   *
+   * @return the comparator used to sort the keys
+   */
   public abstract Comparator<K> getComparator();
 
+  /**
+   * Checks if this map is equal to the specified object.
+   *
+   * @param o the object to compare to
+   * @return {@code true} if this map is equal to the specified object, {@code false} otherwise
+   */
   @Override
   @SuppressWarnings("unchecked")
   public boolean equals(Object o) {
@@ -81,6 +182,11 @@ public abstract class ImmutableSortedMap<K, V> implements Iterable<Map.Entry<K, 
     return true;
   }
 
+  /**
+   * Returns the hash code for this map.
+   *
+   * @return the hash code for this map
+   */
   @Override
   public int hashCode() {
     int result = this.getComparator().hashCode();
@@ -91,6 +197,11 @@ public abstract class ImmutableSortedMap<K, V> implements Iterable<Map.Entry<K, 
     return result;
   }
 
+  /**
+   * Returns a string representation of this map.
+   *
+   * @return a string representation of this map
+   */
   public String toString() {
     StringBuilder b = new StringBuilder();
     b.append(this.getClass().getSimpleName());
