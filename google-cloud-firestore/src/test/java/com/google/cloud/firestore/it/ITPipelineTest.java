4d8a20567a5c1eda4a89eb465508eca03f62b3b9
Wu-Hui <wu.hui.github@gmail.com>
2024-03-07 11:13:02 -0500

    pipeline result basic

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITPipelineTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/it/ITPipelineTest.java
@@ -16,7 +16,6 @@
 
 package com.google.cloud.firestore.it;
 
-
 import static com.google.cloud.firestore.pipeline.Expr.and;
 import static com.google.cloud.firestore.pipeline.Expr.avg;
 import static com.google.cloud.firestore.pipeline.Expr.cosineDistance;
@@ -58,133 +57,133 @@ public class ITPipelineTest {
 
   @Test
   public void projections() throws Exception {
-    Pipeline p = Pipeline.fromCollection("coll1")
-        .project(
-            Field.of("foo"),
-            Constant.of("emptyValue").asAlias("emptyField"),
-            Field.of("embedding").cosineDistance(new double[]{1, 2, 3.0}).asAlias("distance")
-        );
+    Pipeline p =
+        Pipeline.fromCollection("coll1")
+            .project(
+                Field.of("foo"),
+                Constant.of("emptyValue").asAlias("emptyField"),
+                Field.of("embedding").cosineDistance(new double[] {1, 2, 3.0}).asAlias("distance"));
 
     // More compact
-    p = Pipeline.fromCollection("coll1")
-        .project(
-            Fields.of("foo", "bar", "baz"),
-            Constant.of(42).asAlias("emptyField")
-        );
-    p = Pipeline.fromCollection("coll1")
-        // basically an addField
-        .project(
-            Field.ofAll(),
-            Constant.of(42).asAlias("emptyField")
-        );
+    p =
+        Pipeline.fromCollection("coll1")
+            .project(Fields.of("foo", "bar", "baz"), Constant.of(42).asAlias("emptyField"));
+    p =
+        Pipeline.fromCollection("coll1")
+            // basically an addField
+            .project(Field.ofAll(), Constant.of(42).asAlias("emptyField"));
   }
 
   @Test
   public void addRemoveFields() throws Exception {
-    Pipeline p = Pipeline.fromCollection("coll1")
-        .addFields(
-            Constant.of("emptyValue").asAlias("emptyField"),
-            Field.of("embedding").cosineDistance(new double[]{1, 2, 3.0}).asAlias("distance")
-        )
-        .removeFields(Field.of("emptyField"));
+    Pipeline p =
+        Pipeline.fromCollection("coll1")
+            .addFields(
+                Constant.of("emptyValue").asAlias("emptyField"),
+                Field.of("embedding").cosineDistance(new double[] {1, 2, 3.0}).asAlias("distance"))
+            .removeFields(Field.of("emptyField"));
   }
 
   @Test
   public void filters() throws Exception {
-    Pipeline p = Pipeline.fromCollectionGroup("coll1")
-        .filter(Field.of("foo").equal(Constant.of(42)))
-        .filter(or(Field.of("bar").lessThan(Constant.of(100)),
-            Constant.of("value").equal(Field.of("key"))
-        ))
-        .filter(not(Constant.of(128).inAny(Field.of("f1"), Field.of("f2"))));
+    Pipeline p =
+        Pipeline.fromCollectionGroup("coll1")
+            .filter(Field.of("foo").equal(Constant.of(42)))
+            .filter(
+                or(
+                    Field.of("bar").lessThan(Constant.of(100)),
+                    Constant.of("value").equal(Field.of("key"))))
+            .filter(not(Constant.of(128).inAny(Field.of("f1"), Field.of("f2"))));
 
-    p = Pipeline.fromCollectionGroup("coll1")
-        .filter(equal("foo", 42))
-        .filter(or(lessThan("bar", 100),
-            equal("key", Constant.of("value"))
-        ))
-        .filter(not(Constant.of(128).inAny(Field.of("f1"), Field.of("f2"))));
+    p =
+        Pipeline.fromCollectionGroup("coll1")
+            .filter(equal("foo", 42))
+            .filter(or(lessThan("bar", 100), equal("key", Constant.of("value"))))
+            .filter(not(Constant.of(128).inAny(Field.of("f1"), Field.of("f2"))));
   }
 
   @Test
   public void inFilters() throws Exception {
-    Pipeline p = Pipeline.fromCollectionGroup("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")));
+    Pipeline p =
+        Pipeline.fromCollectionGroup("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")));
   }
 
   @Test
   public void groupBy() throws Exception {
-    Pipeline p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")))
-        .group(Fields.of("given_name", "family_name"))
-        .aggregate(avg(Field.of("score")).toField("avg_score_1"))
-        // Equivalent but more fluency
-        .group(Fields.of("given_name", "family_name"))
-        .aggregate(Field.of("score").avg().toField("avg_score_2"));
+    Pipeline p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")))
+            .group(Fields.of("given_name", "family_name"))
+            .aggregate(avg(Field.of("score")).toField("avg_score_1"))
+            // Equivalent but more fluency
+            .group(Fields.of("given_name", "family_name"))
+            .aggregate(Field.of("score").avg().toField("avg_score_2"));
   }
 
   @Test
   public void aggregateWithoutGrouping() throws Exception {
-    Pipeline p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")))
-        .aggregate(avg(Field.of("score")).toField("avg_score_1"));
+    Pipeline p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")))
+            .aggregate(avg(Field.of("score")).toField("avg_score_1"));
   }
 
   @Test
   public void joins() throws Exception {
-    Pipeline p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")));
-    Pipeline pipe = Pipeline.fromCollection("users")
-        .findNearest(Field.of("embedding"), new double[]{1.0, 2.0},
-            new FindNearestOptions(Similarity.euclidean(), 1000, Field.of("distance")))
-        .innerJoin(p)
-        .on(and(
-            Field.of("foo").equal(p.fieldOf("bar")),
-            p.fieldOf("requirement").greaterThan(Field.of("distance"))));
+    Pipeline p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")));
+    Pipeline pipe =
+        Pipeline.fromCollection("users")
+            .findNearest(
+                Field.of("embedding"),
+                new double[] {1.0, 2.0},
+                new FindNearestOptions(Similarity.euclidean(), 1000, Field.of("distance")))
+            .innerJoin(p)
+            .on(
+                and(
+                    Field.of("foo").equal(p.fieldOf("bar")),
+                    p.fieldOf("requirement").greaterThan(Field.of("distance"))));
 
-    Pipeline another = Pipeline.fromCollection("users")
-        .innerJoin(p)
-        .on(Fields.of("foo", "bar"))
-        .project(Field.ofAll().withPrefix("left"), p.fieldOfAll().withPrefix("right"));
+    Pipeline another =
+        Pipeline.fromCollection("users")
+            .innerJoin(p)
+            .on(Fields.of("foo", "bar"))
+            .project(Field.ofAll().withPrefix("left"), p.fieldOfAll().withPrefix("right"));
   }
 
   @Test
   public void sorts() throws Exception {
-    Pipeline p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")))
-        .sort(Ordering.of(Field.of("rank")),
-            Ordering.of(cosineDistance(Field.of("embedding1"), Field.of("embedding2")),
-                Direction.DESC))
-        .limit(100);
+    Pipeline p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")))
+            .sort(
+                Ordering.of(Field.of("rank")),
+                Ordering.of(
+                    cosineDistance(Field.of("embedding1"), Field.of("embedding2")), Direction.DESC))
+            .limit(100);
 
     // equivalent but more concise.
-    p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")))
-        .sort(ascending(Field.of("rank")),
-            descending(cosineDistance(Field.of("embedding1"), Field.of("embedding2"))))
-        .limit(100);
+    p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")))
+            .sort(
+                ascending(Field.of("rank")),
+                descending(cosineDistance(Field.of("embedding1"), Field.of("embedding2"))))
+            .limit(100);
   }
 
   @Test
   public void pagination() throws Exception {
-    PaginatingPipeline p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")))
-        .paginate(100, Ordering.of(cosineDistance(Field.of("embedding1"), Field.of("embedding2")),
-            Direction.DESC));
+    PaginatingPipeline p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")))
+            .paginate(
+                100,
+                Ordering.of(
+                    cosineDistance(Field.of("embedding1"), Field.of("embedding2")),
+                    Direction.DESC));
 
     PipelineResult result = firestore.execute(p.firstPage()).get();
     PipelineResult second = firestore.execute(p.startAfter(result)).get();
@@ -194,12 +193,14 @@ public class ITPipelineTest {
 
   @Test
   public void fluentAllTheWay() throws Exception {
-    PaginatingPipeline p = Pipeline.fromCollection("coll1")
-        .filter(Field.of("foo").inAny(
-            Constant.of(42),
-            Field.of("bar")))
-        .paginate(100, Ordering.of(cosineDistance(Field.of("embedding1"), Field.of("embedding2")),
-            Direction.DESC));
+    PaginatingPipeline p =
+        Pipeline.fromCollection("coll1")
+            .filter(Field.of("foo").inAny(Constant.of(42), Field.of("bar")))
+            .paginate(
+                100,
+                Ordering.of(
+                    cosineDistance(Field.of("embedding1"), Field.of("embedding2")),
+                    Direction.DESC));
 
     PipelineResult result = p.firstPage().execute(firestore).get();
     PipelineResult second = p.startAfter(result).execute(firestore).get();
