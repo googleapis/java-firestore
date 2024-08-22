f4b8416aed326d3290aaf6db01cd6cda920440c6
milaGGL <107142260+milaGGL@users.noreply.github.com>
2024-08-22 10:35:20 -0400

    move constants out

---
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/telemetry/BuiltinMetricsConstants.java
@@ -0,0 +1,139 @@
+package com.google.cloud.firestore.telemetry;
+
+import com.google.api.gax.tracing.OpenTelemetryMetricsRecorder;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.sdk.metrics.InstrumentSelector;
+import io.opentelemetry.sdk.metrics.View;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BuiltinMetricsConstants {
+
+  static final String FIRESTORE_METER_NAME = "firestore_java";
+
+  // TODO: change to firestore.googleapis.com
+  public static final String METER_NAME = "custom.googleapis.com/internal/client";
+  public static final String GAX_METER_NAME = OpenTelemetryMetricsRecorder.GAX_METER_NAME;
+
+  // Metric attribute keys for monitored resource
+  static final AttributeKey<String> PROJECT_ID_KEY = AttributeKey.stringKey("project_id");
+  // public static final AttributeKey<String> INSTANCE_ID_KEY = AttributeKey.stringKey("instance");
+
+  // Metric attribute keys for labels
+  // static final AttributeKey<String> LANGUAGE_KEY = AttributeKey.stringKey("language");
+  static final AttributeKey<String> METHOD_NAME_KEY = AttributeKey.stringKey("method_name");
+  static final AttributeKey<String> STATUS_KEY = AttributeKey.stringKey("status");
+  public static final AttributeKey<String> DATABASE_ID_KEY = AttributeKey.stringKey("database_id");
+  static final AttributeKey<String> CLIENT_LIBRARY_KEY = AttributeKey.stringKey("client_library");
+  static final AttributeKey<String> LIBRARY_VERSION_KEY = AttributeKey.stringKey("library_version");
+  static final AttributeKey<String> CLIENT_UID_KEY = AttributeKey.stringKey("client_uid");
+
+  static final AttributeKey<String> Language_KEY = AttributeKey.stringKey("language");
+  public static final AttributeKey<String> SERVICE_NAME = AttributeKey.stringKey("service.name");
+  // Metric
+  static final String OPERATION_LATENCY_NAME = "operation_latency";
+  static final String OPERATION_COUNT_NAME = "operation_count";
+  static final String ATTEMPT_LATENCY_NAME = "attempt_latency";
+  static final String ATTEMPT_COUNT_NAME = "attempt_count";
+  static final String FIRST_RESPONSE_LATENCY_NAME = "first_response_latency";
+  static final String END_TO_END_LATENCY_NAME = "end_to_end_latency";
+  static final String TRANSACTION_LATENCY_NAME = "transaction_latency";
+  static final String TRANSACTION_ATTEMPT_COUNT_NAME = "transaction_attempt_count";
+
+  static final String MILLISECOND_UNIT = "ms";
+
+  public static final String ENABLE_METRICS_ENV_VAR = "FIRESTORE_ENABLE_TRACING";
+
+  public static final Set<AttributeKey> COMMON_ATTRIBUTES =
+      ImmutableSet.of(
+          PROJECT_ID_KEY,
+          DATABASE_ID_KEY,
+          CLIENT_UID_KEY,
+          CLIENT_LIBRARY_KEY,
+          LIBRARY_VERSION_KEY,
+          STATUS_KEY);
+
+  public static final Set<String> BUILTIN_METRICS =
+      ImmutableSet.of(
+              OPERATION_LATENCY_NAME,
+              ATTEMPT_LATENCY_NAME,
+              OPERATION_COUNT_NAME,
+              ATTEMPT_COUNT_NAME,
+              FIRST_RESPONSE_LATENCY_NAME,
+              END_TO_END_LATENCY_NAME,
+              TRANSACTION_LATENCY_NAME,
+              TRANSACTION_ATTEMPT_COUNT_NAME)
+          .stream()
+          .map(m -> METER_NAME + '/' + m)
+          .collect(Collectors.toSet());
+
+  static void defineView(
+      ImmutableMap.Builder<InstrumentSelector, View> viewMap,
+      String id,
+      Set<AttributeKey> attributes) {
+    InstrumentSelector selector =
+        InstrumentSelector.builder().setName(METER_NAME + "/" + id).build();
+    Set<String> attributesFilter =
+        ImmutableSet.<String>builder()
+            .addAll(attributes.stream().map(AttributeKey::getKey).collect(Collectors.toSet()))
+            .build();
+    View view = View.builder().setAttributeFilter(attributesFilter).build();
+
+    viewMap.put(selector, view);
+  }
+
+  public static Map<InstrumentSelector, View> getAllViews() {
+    ImmutableMap.Builder<InstrumentSelector, View> views = ImmutableMap.builder();
+
+    defineView(
+        views,
+        OPERATION_LATENCY_NAME,
+        ImmutableSet.<AttributeKey>builder()
+            .addAll(COMMON_ATTRIBUTES)
+            .add(METHOD_NAME_KEY)
+            .build());
+    defineView(
+        views,
+        ATTEMPT_LATENCY_NAME,
+        ImmutableSet.<AttributeKey>builder()
+            .addAll(COMMON_ATTRIBUTES)
+            .add(METHOD_NAME_KEY)
+            .build());
+
+    defineView(
+        views,
+        OPERATION_COUNT_NAME,
+        ImmutableSet.<AttributeKey>builder()
+            .addAll(COMMON_ATTRIBUTES)
+            .add(METHOD_NAME_KEY)
+            .build());
+
+    defineView(
+        views,
+        ATTEMPT_COUNT_NAME,
+        ImmutableSet.<AttributeKey>builder()
+            .addAll(COMMON_ATTRIBUTES)
+            .add(METHOD_NAME_KEY)
+            .build());
+
+    defineView(
+        views,
+        FIRST_RESPONSE_LATENCY_NAME,
+        ImmutableSet.<AttributeKey>builder()
+            .addAll(COMMON_ATTRIBUTES)
+            .add(METHOD_NAME_KEY)
+            .build());
+
+    defineView(
+        views,
+        END_TO_END_LATENCY_NAME,
+        ImmutableSet.<AttributeKey>builder()
+            .addAll(COMMON_ATTRIBUTES)
+            .add(METHOD_NAME_KEY)
+            .build());
+    return views.build();
+  }
+}
--- google-cloud-firestore/src/main/java/com/google/cloud/firestore/telemetry/MetricsUtil.java
+++ google-cloud-firestore/src/main/java/com/google/cloud/firestore/telemetry/MetricsUtil.java
@@ -16,6 +16,8 @@
 
 package com.google.cloud.firestore.telemetry;
 
+import static com.google.cloud.firestore.telemetry.BuiltinMetricsConstants.*;
+
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
@@ -25,30 +27,19 @@ import com.google.api.gax.tracing.MetricsTracerFactory;
 import com.google.api.gax.tracing.OpenTelemetryMetricsRecorder;
 import com.google.cloud.firestore.FirestoreException;
 import com.google.cloud.firestore.FirestoreOptions;
-import com.google.cloud.opentelemetry.metric.GoogleCloudMetricExporter;
-import com.google.cloud.opentelemetry.metric.MetricConfiguration;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.MoreExecutors;
 import io.grpc.Status;
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.metrics.InstrumentSelector;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
-import io.opentelemetry.sdk.metrics.View;
-import io.opentelemetry.sdk.metrics.export.MetricExporter;
-import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.resources.Resource;
+import java.io.IOException;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 /**
@@ -57,63 +48,6 @@ import javax.annotation.Nullable;
  * they may use a particular tracing framework such as OpenTelemetry.
  */
 public class MetricsUtil {
-  static final String FIRESTORE_METER_NAME = "firestore_java";
-
-  // TODO: change to firestore.googleapis.com
-  public static final String METER_NAME = "custom.googleapis.com/internal/client";
-  public static final String GAX_METER_NAME = OpenTelemetryMetricsRecorder.GAX_METER_NAME;
-
-  // Metric attribute keys for monitored resource
-  static final AttributeKey<String> PROJECT_ID_KEY = AttributeKey.stringKey("project_id");
-  // public static final AttributeKey<String> INSTANCE_ID_KEY = AttributeKey.stringKey("instance");
-
-  // Metric attribute keys for labels
-  // static final AttributeKey<String> LANGUAGE_KEY = AttributeKey.stringKey("language");
-  static final AttributeKey<String> METHOD_NAME_KEY = AttributeKey.stringKey("method_name");
-  static final AttributeKey<String> STATUS_KEY = AttributeKey.stringKey("status");
-  public static final AttributeKey<String> DATABASE_ID_KEY = AttributeKey.stringKey("database_id");
-  static final AttributeKey<String> CLIENT_LIBRARY_KEY = AttributeKey.stringKey("client_library");
-  static final AttributeKey<String> LIBRARY_VERSION_KEY = AttributeKey.stringKey("library_version");
-  static final AttributeKey<String> CLIENT_UID_KEY = AttributeKey.stringKey("client_uid");
-
-  static final AttributeKey<String> Language_KEY = AttributeKey.stringKey("language");
-  public static final AttributeKey<String> SERVICE_NAME = AttributeKey.stringKey("service.name");
-  // Metric
-  static final String OPERATION_LATENCY_NAME = "operation_latency";
-  static final String OPERATION_COUNT_NAME = "operation_count";
-  static final String ATTEMPT_LATENCY_NAME = "attempt_latency";
-  static final String ATTEMPT_COUNT_NAME = "attempt_count";
-  static final String FIRST_RESPONSE_LATENCY_NAME = "first_response_latency";
-  static final String END_TO_END_LATENCY_NAME = "end_to_end_latency";
-  static final String TRANSACTION_LATENCY_NAME = "transaction_latency";
-  static final String TRANSACTION_ATTEMPT_COUNT_NAME = "transaction_attempt_count";
-
-  static final String MILLISECOND_UNIT = "ms";
-
-  static final String ENABLE_METRICS_ENV_VAR = "FIRESTORE_ENABLE_TRACING";
-
-  public static final Set<AttributeKey> COMMON_ATTRIBUTES =
-      ImmutableSet.of(
-          PROJECT_ID_KEY,
-          DATABASE_ID_KEY,
-          CLIENT_UID_KEY,
-          CLIENT_LIBRARY_KEY,
-          LIBRARY_VERSION_KEY,
-          STATUS_KEY);
-
-  public static final Set<String> BUILTIN_METRICS =
-      ImmutableSet.of(
-              OPERATION_LATENCY_NAME,
-              ATTEMPT_LATENCY_NAME,
-              OPERATION_COUNT_NAME,
-              ATTEMPT_COUNT_NAME,
-              FIRST_RESPONSE_LATENCY_NAME,
-              END_TO_END_LATENCY_NAME,
-              TRANSACTION_LATENCY_NAME,
-              TRANSACTION_ATTEMPT_COUNT_NAME)
-          .stream()
-          .map(m -> METER_NAME + '/' + m)
-          .collect(Collectors.toSet());
 
   private final FirestoreOptions firestoreOptions;
   private OpenTelemetry openTelemetry;
@@ -149,11 +83,15 @@ public class MetricsUtil {
     }
 
     if (createEnabledInstance) {
-      createMetricsUtil();
+      try {
+        createMetricsUtil();
+      } catch (IOException e) {
+        System.out.println(e);
+      }
     }
   }
 
-  private void createMetricsUtil() {
+  private void createMetricsUtil() throws IOException {
     this.openTelemetry = getDefaultOpenTelemetryInstance();
 
     OpenTelemetryMetricsRecorder recorder =
@@ -194,7 +132,7 @@ public class MetricsUtil {
     }
   }
 
-  private OpenTelemetry getDefaultOpenTelemetryInstance() {
+  private OpenTelemetry getDefaultOpenTelemetryInstance() throws IOException {
     OpenTelemetry openTelemetry = firestoreOptions.getOpenTelemetryOptions().getOpenTelemetry();
 
     // If metrics is enabled, but an OpenTelemetry instance is not provided, create a default OTel
@@ -202,28 +140,8 @@ public class MetricsUtil {
     if (openTelemetry == null) {
       SdkMeterProviderBuilder sdkMeterProviderBuilder = SdkMeterProvider.builder();
 
-      // Attach built-in exporter
-      MetricExporter metricExporter =
-          GoogleCloudMetricExporter.createWithConfiguration(
-              MetricConfiguration.builder().setProjectId(firestoreOptions.getProjectId()).build());
-      sdkMeterProviderBuilder.registerMetricReader(
-          PeriodicMetricReader.builder(metricExporter).build());
-
-      // Add static attributes to resource
-      Package pkg = this.getClass().getPackage();
-      sdkMeterProviderBuilder.setResource(
-          Resource.getDefault()
-              .merge(
-                  Resource.builder()
-                      .put(DATABASE_ID_KEY, firestoreOptions.getDatabaseId())
-                      .put(CLIENT_LIBRARY_KEY, FIRESTORE_METER_NAME)
-                      .put(LIBRARY_VERSION_KEY, pkg.getImplementationVersion())
-                      .build()));
-
-      // Filter out attributes that are not defined
-      for (Map.Entry<InstrumentSelector, View> entry : getAllViews().entrySet()) {
-        sdkMeterProviderBuilder.registerView(entry.getKey(), entry.getValue());
-      }
+      BuiltinMetricsView.registerBuiltinMetrics(
+          firestoreOptions.getProjectId(), sdkMeterProviderBuilder);
 
       openTelemetry =
           OpenTelemetrySdk.builder().setMeterProvider(sdkMeterProviderBuilder.build()).build();
@@ -282,72 +200,5 @@ public class MetricsUtil {
     Status status = ((FirestoreException) throwable).getStatus();
 
     return status.getCode().name();
-  }
-
-  static void defineView(
-      ImmutableMap.Builder<InstrumentSelector, View> viewMap,
-      String id,
-      Set<AttributeKey> attributes) {
-    InstrumentSelector selector =
-        InstrumentSelector.builder().setName(METER_NAME + "/" + id).build();
-    Set<String> attributesFilter =
-        ImmutableSet.<String>builder()
-            .addAll(attributes.stream().map(AttributeKey::getKey).collect(Collectors.toSet()))
-            .build();
-    View view = View.builder().setAttributeFilter(attributesFilter).build();
-
-    viewMap.put(selector, view);
-  }
-
-  public static Map<InstrumentSelector, View> getAllViews() {
-    ImmutableMap.Builder<InstrumentSelector, View> views = ImmutableMap.builder();
-
-    defineView(
-        views,
-        OPERATION_LATENCY_NAME,
-        ImmutableSet.<AttributeKey>builder()
-            .addAll(COMMON_ATTRIBUTES)
-            .add(METHOD_NAME_KEY)
-            .build());
-    defineView(
-        views,
-        ATTEMPT_LATENCY_NAME,
-        ImmutableSet.<AttributeKey>builder()
-            .addAll(COMMON_ATTRIBUTES)
-            .add(METHOD_NAME_KEY)
-            .build());
-
-    defineView(
-        views,
-        OPERATION_COUNT_NAME,
-        ImmutableSet.<AttributeKey>builder()
-            .addAll(COMMON_ATTRIBUTES)
-            .add(METHOD_NAME_KEY)
-            .build());
-
-    defineView(
-        views,
-        ATTEMPT_COUNT_NAME,
-        ImmutableSet.<AttributeKey>builder()
-            .addAll(COMMON_ATTRIBUTES)
-            .add(METHOD_NAME_KEY)
-            .build());
-
-    defineView(
-        views,
-        FIRST_RESPONSE_LATENCY_NAME,
-        ImmutableSet.<AttributeKey>builder()
-            .addAll(COMMON_ATTRIBUTES)
-            .add(METHOD_NAME_KEY)
-            .build());
-
-    defineView(
-        views,
-        END_TO_END_LATENCY_NAME,
-        ImmutableSet.<AttributeKey>builder()
-            .addAll(COMMON_ATTRIBUTES)
-            .add(METHOD_NAME_KEY)
-            .build());
-    return views.build();
   }
 }
