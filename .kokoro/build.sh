e8ec9540f06fc7073d2143312926fec16ba78b5b
Lawrence Qiu <lawrenceqiu@google.com>
2025-04-24 13:31:59 -0400

    chore: Test with Protobuf-Java v4.29.4

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -67,6 +67,7 @@ javadoc)
 integration)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -ntp \
+      -Dprotobuf.version=4.29.4 \
       -Penable-integration-tests \
       -DtrimStackTrace=false \
       -Dclirr.skip=true \
