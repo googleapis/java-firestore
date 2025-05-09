78008c3b951adbac5e721dae524756128d3e7fdb
milaGGL <107142260+milaGGL@users.noreply.github.com>
2025-05-09 10:14:18 -0400

    resolve comments

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/FirestoreExceptionTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/FirestoreExceptionTest.java
@@ -16,7 +16,10 @@
 
 package com.google.cloud.firestore;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import com.google.api.gax.grpc.GrpcStatusCode;
 import com.google.api.gax.rpc.ApiException;
