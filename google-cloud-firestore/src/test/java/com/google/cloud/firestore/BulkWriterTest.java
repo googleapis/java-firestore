36d0f2fb75d87798094198df1e2b8ca3e3e8b3f5
Blake Li <blakeli@google.com>
2025-12-12 14:48:20 -0500

    tests: Ignore flaky test in BulkWriterTest

--- google-cloud-firestore/src/test/java/com/google/cloud/firestore/BulkWriterTest.java
+++ google-cloud-firestore/src/test/java/com/google/cloud/firestore/BulkWriterTest.java
@@ -63,6 +63,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -1279,6 +1280,7 @@ public class BulkWriterTest {
   }
 
   @Test
+  @Ignore
   public void sendsBackoffBatchAfterOtherEnqueuedBatches() throws Exception {
     ResponseStubber responseStubber =
         new ResponseStubber() {
